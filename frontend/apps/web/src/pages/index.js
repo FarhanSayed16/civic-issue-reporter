98ab8675a50f220034febb643c88b24b0ab8506a
Farhan <farhansayed54@gmail.com>
2025-09-22 20:31:51 +0530

    feat: add ProfilePage component for user profile management and update functionality

---
+++ frontend/apps/web/src/pages/index.js
@@ -0,0 +1,6 @@
+import HomePage from "./HomePage";
+import SignupPage from "./SignupPage";
+import LoginPage from "./LoginPage";
+import ProfilePage from "./ProfilePage";
+
+export {SignupPage,LoginPage,ProfilePage,HomePage,};
